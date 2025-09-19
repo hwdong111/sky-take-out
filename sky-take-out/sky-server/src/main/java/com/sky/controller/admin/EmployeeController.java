c7b08688b634fa72b2dcd55755ceedb7c68dcf7c
hw dong <3038470295@qq.com>
2025-09-19 23:59:37 +0800

    启用禁用员工账号代码开发

--- sky-take-out/sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-take-out/sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -91,7 +91,19 @@ public class EmployeeController {
         log.info("分页查询员工：{}", employeePageQueryDTO);
         PageResult pageResult = employeeService.pageQuery(employeePageQueryDTO);
         return Result.success(pageResult);
+    }
 
+    /**
+     * 启用或禁用员工账号
+     * @param status
+     * @param id
+     * @return
+     */
+    @PostMapping("/status/{status}")
+    @ApiOperation("启用或禁用员工账号")
+    public Result startOrStop(@PathVariable Integer status, Long id){
+        employeeService.startOrStop(status, id);
+        return Result.success();
     }
 
 }
