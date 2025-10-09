f3bf5cc0168bb54ef2fad9d61c55d92993e14936
hw dong <3038470295@qq.com>
2025-10-09 20:06:05 +0800

    销量排名统计功能代码开发

--- sky-take-out/sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-take-out/sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -3,6 +3,7 @@ package com.sky.controller.admin;
 import com.sky.result.Result;
 import com.sky.service.ReportService;
 import com.sky.vo.OrderReportVO;
+import com.sky.vo.SalesTop10ReportVO;
 import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 import io.swagger.annotations.Api;
@@ -61,11 +62,25 @@ public class ReportController {
      * @param end
      * @return
      */
-   @GetMapping("/ordersStatistics")
+    @GetMapping("/ordersStatistics")
     @ApiOperation("订单统计")
     public Result<OrderReportVO> ordersStatistics(
            @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate begin,
            @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end){
         return Result.success(reportService.getOrderStatistics(begin, end));
+    }
+
+    /**
+     * 销售排行
+     * @param begin
+     * @param end
+     * @return
+     */
+    @GetMapping("/top10")
+    @ApiOperation("销量排名top10")
+    public Result<SalesTop10ReportVO> top10(
+            @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate begin,
+            @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end){
+        return Result.success(reportService.getSalesTop10(begin, end));
     }
 }
--- sky-take-out/sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-take-out/sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -1,6 +1,7 @@
 package com.sky.mapper;
 
 import com.github.pagehelper.Page;
+import com.sky.dto.GoodsSalesDTO;
 import com.sky.dto.OrdersPageQueryDTO;
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
@@ -87,4 +88,12 @@ public interface OrderMapper {
      * @return
      */
     Integer countByMap(Map map);
+
+    /**
+     * 统计指定时间区间内的销量排名top10
+     * @param beginTime
+     * @param endTime
+     * @return
+     */
+    List<GoodsSalesDTO> getSalesTop10(LocalDateTime beginTime, LocalDateTime endTime);
 }
